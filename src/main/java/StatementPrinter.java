2595fd8045058e542948ff820c882d865d27ad15
LeSecret <arnaudnjr1998@gmail.com>
2023-10-26 13:20:04 +0300

    Ajout des commentaires

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -8,6 +8,7 @@ public class StatementPrinter {
     int volumeCredits = 0;
     StringBuffer result = new StringBuffer("Statement for " + invoice.customer + "\n");
 
+    // Format de devise
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
@@ -16,12 +17,14 @@ public class StatementPrinter {
 
       switch (play.type) {
         case "tragedy":
+          // Calcul du montant pour une pièce de type "tragedy"
           thisAmount = 40000;
           if (perf.audience > 30) {
             thisAmount += 1000 * (perf.audience - 30);
           }
           break;
         case "comedy":
+          // Calcul du montant pour une pièce de type "comedy"
           thisAmount = 30000;
           if (perf.audience > 20) {
             thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -41,8 +44,12 @@ public class StatementPrinter {
       result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
       totalAmount += thisAmount;
     }
+
+    // Ajout du montant total et des crédits de volume à la chaîne résultante
     result.append("Amount owed is " + frmt.format(totalAmount / 100) + "\n");
     result.append("You earned " + volumeCredits + " credits\n");
+    
+    // Conversion du StringBuffer en chaîne de caractères et retour
     return result.toString();
   }
 
